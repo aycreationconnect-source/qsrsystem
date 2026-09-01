574bac8a8ec1c32c4081b825d142f1978c385ecb
aycreationconnect-source <aycreationconnect@gmail.com>
2026-09-01 13:01:28 +0530

    feat: initialize backend project with NestJS and Prisma MariaDB integration

--- backend/src/prisma/prisma.service.ts
+++ backend/src/prisma/prisma.service.ts
@@ -1,19 +1,17 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 import { PrismaMariaDb } from '@prisma/adapter-mariadb';
-import * as mariadb from 'mariadb';
-
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
   constructor() {
     const adapter = new PrismaMariaDb({
-      host: 'localhost',
-      port: 3306,
-      user: 'root',
-      password: 'Aniket@123',
-      database: 'qsr_db',
-      connectionLimit: 10,
-      allowPublicKeyRetrieval: true
+      host: process.env.DATABASE_HOST!,
+      port: Number(process.env.DATABASE_PORT!),
+      user: process.env.DATABASE_USER!,
+      password: process.env.DATABASE_PASSWORD!,
+      database: process.env.DATABASE_NAME!,
+      connectionLimit: Number(process.env.DATABASE_CONNECTION_LIMIT!),
+      allowPublicKeyRetrieval: true,
     });
     super({ adapter });
   }
